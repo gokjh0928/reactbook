3e341b332e4bf62978f9929e29b908bfa2e80dbe
zaraconsulting <derek@zaraconsulting.org>
2021-07-19 12:16:10 -0500

    starter app w/ routing

---
+++ src/views/Main.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <Navbar />
+                </header>
+
+                <main>
+                    <Switch>
+                        <Route path={'/'} render={() => <Home />} />
+                    </Switch>
+                </main>
+
+                <footer>
+
+                </footer>
+            </div>
+        )
+    }
+}
